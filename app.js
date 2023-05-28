2bd7fa547bdcff0f53f7d660fd53740227106610
Kirill Osterov <43790130+Ziviy@users.noreply.github.com>
2023-05-29 01:56:03 +0300

    Мердж файлов

--- app.js
+++ app.js
@@ -1,8 +1,143 @@
-const generation = require('./generation');
+//const gen = require('./generation');
+const fs = require('fs');
+const readline = require('readline');
+
+
+let chunkIndex = 0;
+
+const inputFile = 'Files/originalFile.txt';
+const outputFile = 'Files/sortedFile.txt';
+
+const chunkSize = 30 * 1024 * 1024; //1mb
+
+async function SortFile() {
+    await ReadFile();
+    await mergeChunks(chunkIndex);
+    //await DeleteChunks(chunkIndex);
+}
+
+async function ReadFile() {
+    let chunk = [];
+    let currentChunkSize = 0;
+
+    const readStream = fs.createReadStream(inputFile, { encoding: 'utf-8' });
+    const rl = readline.createInterface({
+        input: readStream,
+        crlfDelay: Infinity
+    });
+
+
+    for await (const line of rl) {
+        chunk.push(line);
+
+        currentChunkSize += line.length;
+        if (currentChunkSize >= chunkSize) {
+            writeChunk(chunk);
+            
+            chunk = [];
+            currentChunkSize = 0;
+        }
+    }
+
+    if (currentChunkSize > 0) {
+        writeChunk(chunk);
+        chunk = [];
+        currentChunkSize = 0;
+    }
+}
+
+function writeChunk(chunk) {
+    chunk.sort();
+    const writeStream = fs.createWriteStream(`Files/chunk_${chunkIndex}.txt`);
+    chunk.forEach(function(v) {
+        writeStream.write(v + '\n');
+    })
+    
+    writeStream.end();
+
+    
+    chunkIndex++;
+}
+
+async function mergeChunks() {
+    const chunkFiles = await findChunkFiles();
+    const writeStream = fs.createWriteStream(outputFile, { encoding: 'utf-8' });
+  
+    for (const chunkFile of chunkFiles) {
+      await mergeChunkFile(chunkFile, writeStream);
+    }
+  
+    writeStream.end();
+    console.log('Chunk files merged and sorted successfully!');
+  
+    await deleteChunkFiles(chunkFiles);
+    console.log('Chunk files deleted successfully!');
+  }
+  
+  async function findChunkFiles() {
+    return new Promise((resolve, reject) => {
+      fs.readdir('Files', (error, files) => {
+        if (error) {
+          reject(error);
+        } else {
+          const chunkFiles = files.filter((file) => file.startsWith('chunk_'));
+          resolve(chunkFiles);
+        }
+      });
+    });
+  }
+  
+  async function mergeChunkFile(chunkFile, writeStream) {
+    return new Promise((resolve, reject) => {
+      const readStream = fs.createReadStream(`Files/${chunkFile}`, { encoding: 'utf-8' });
+      const rl = readline.createInterface({
+        input: readStream,
+        crlfDelay: Infinity
+      });
+  
+      rl.on('line', (line) => {
+        writeStream.write(line + '\n');
+      });
+  
+      rl.on('close', () => {
+        resolve();
+      });
+  
+      rl.on('error', (error) => {
+        reject(error);
+      });
+    });
+  }
+  
+  async function deleteChunkFiles(chunkFiles) {
+    return new Promise((resolve, reject) => {
+      let deletedCount = 0;
+  
+      for (const chunkFile of chunkFiles) {
+        fs.unlink(`Files/${chunkFile}`, (error) => {
+          if (error) {
+            reject(error);
+          } else {
+            deletedCount++;
+            if (deletedCount === chunkFiles.length) {
+              resolve();
+            }
+          }
+        });
+      }
+    });
+  }
 
 // 10000000 - 187mb
 // 100000000 - 1.87gb
 // 1000000000 - 10.87gb
-generation.generateFileWithRows(10000000);
+
+
+//gen.generateFileWithRows(100000000);
+SortFile();
+
+// const used = process.memoryUsage();
+// console.log(`Memory usage: ${Math.round(used.heapUsed / 1024 / 1024 * 100) / 100} MB`);
+
 
 
--- generation.js
+++ generation.js
@@ -2,8 +2,13 @@ const fs = require('fs');
 const crypto = require('crypto');
 
 
+const characters = '123456789';
+//const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@';
+
+const writeStream = fs.createWriteStream('Files/originalFile.txt', 'utf-8');
+
 function generateRandomString() {
-    const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@';
+
     let randomString = '';
 
     let max = 30;
@@ -11,34 +16,36 @@ function generateRandomString() {
     let length = Math.floor(Math.random() * (max - min) + min);
 
     for (let i = 0; i < length; i++) {
-        const randomIndex = Math.floor(Math.random() * characters.length);
+        let randomIndex = Math.floor(Math.random() * characters.length);
         randomString += characters.charAt(randomIndex);
     }
 
     return randomString;
 };
 
-function generateFileWithRows(numRows) {
-    const writeStream = fs.createWriteStream('File.txt');
+function generateFileWithRows(numRows, callback) {
+    let i = numRows;
+    write();
+    function write() {
+        let ok = true;
+        do {
+            i -= 1;
+            if (i === 0) {
+                writeStream.write(generateRandomString() + '\n', callback);
+            } else {
+                ok = writeStream.write(generateRandomString() + '\n');
+            }
+        } while (i > 0 && ok);
 
-    for (let i = 0; i < numRows; i++) {
-        const randomString = generateRandomString();
-        writeStream.write(randomString + '\n');
+        if (i > 0) {
+            writeStream.once('drain', write);
+        }
     }
 
-    writeStream.end();
-
-    writeStream.on('finish', () => {
-        console.log('File generation complete.');
-    });
-
-    writeStream.on('error', (err) => {
-        console.error('Error generating file:', err);
-    });
 }
 
-    module.exports = {
-        generateFileWithRows,
-        generateRandomString
-    };
-    
+
+module.exports = {
+    generateFileWithRows,
+    generateRandomString
+};
